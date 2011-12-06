9b36980f35111a36bd47bab32a279f1cb1f27e5a
Muhammad Syaiful Sabril <syaiful.sabril@gmail.com>
2011-12-06 12:46:32 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pdi::Application.config.secret_token = '6f4c3408d31d430d214cbaae5fdeb0912a8f2e56c6d44faa7a1971c4048d9db99a9e96dbd678212d94b7db6f5743c4963dad6a68a01c93be53833b5dbc0a5675'
